f9ed4e3b0fb3c75eede9ab7e7f737ac6b0e38759
= <sandeeppal7902@gmail.com>
2022-11-29 19:34:29 +0530

    npm start

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("./config/database").connect()
+require("./config/database").connect();
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/routes.js')
