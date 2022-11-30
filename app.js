5337caa09a4e39a6f98d214dd86db9ec463ac57c
= <sandeeppal7902@gmail.com>
2022-11-30 08:31:02 +0530

    config database

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require("./config/database").connect();
+require("./Config/database").connect();
+
+
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/routes.js')
